7706165b118248e99e8b175a22fa5ef8e809b4a7
Andrii Suboch <andrii.suboch@gmail.com>
2020-12-12 12:58:34 +0200

    created using react hooks and js functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "./Components/Button";
+import Stopwatch from "./Components/Stopwatch";
+import "./App.css";
 
 function App() {
+  const [time, setTime] = useState({ s: 0, m: 0, h: 0 });
+  const [intervalId, setIntervalId] = useState();
+  const [status, setStatus] = useState(0);
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setIntervalId(setInterval(run, 1000));
+  };
+
+  let updatedS = time.s,
+    updatedM = time.m,
+    updatedH = time.h;
+
+  const run = () => {
+    updatedS++;
+
+    if (updatedS === 60) {
+      updatedS = 0;
+      updatedM++;
+    }
+
+    if (updatedM === 60) {
+      updatedH++;
+      updatedM = 0;
+    }
+
+    return setTime({ s: updatedS, m: updatedM, h: updatedH });
+  };
+
+  const stop = () => {
+    clearInterval(intervalId);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(intervalId);
+    setStatus(0);
+    setTime({ s: 0, m: 0, h: 0 });
+  };
+
+  const resume = () => start();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-section">
+      <div className="clock-holder">
+        <div className="stopwatch">
+          <Stopwatch time={time} />
+          <Button
+            status={status}
+            resume={resume}
+            reset={reset}
+            stop={stop}
+            start={start}
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Stopwatch.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Stopwatch({ time }) {
+  return (
+    <div>
+      <span>{time.h >= 10 ? time.h : "0" + time.h}</span>&nbsp;:&nbsp;
+      <span>{time.m >= 10 ? time.m : "0" + time.m}</span>&nbsp;:&nbsp;
+      <span>{time.s >= 10 ? time.s : "0" + time.s}</span>
+    </div>
+  );
+}
+
+export default Stopwatch;
